20fb03bc9dca40cfca9dcc61d648cc692f5eff3a
Night-Parrot <youdewo@163.com>
2020-04-19 18:57:17 +0800

    onlyoffice页面

---
+++ src/components/edit_tool.vue
@@ -0,0 +1,21 @@
+<template>
+    <iframe id="edit_page" style="height:95%;width:100%;border:0;"></iframe>
+</template>
+
+<script>
+export default {
+    mounted() {
+        this.ylmc = this.$router.history.current.query.ylmc;
+        this.ylbh = this.$router.history.current.query.ylbh;
+        // document.getElementById('edit_page').src = 'http://172.18.17.116/apitest/edit_page/' + this.ylbh + '/' + this.ylmc
+        document.getElementById('edit_page').src = 'http://172.18.48.41:8585/edit_page/' + this.ylbh + '/' + this.ylmc
+    },
+    data() {
+        return {
+           ylbh: '',
+           ylmc: '',
+        }
+    }
+}
+</script>
+
