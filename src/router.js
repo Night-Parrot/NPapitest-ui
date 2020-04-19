db86004d2fcf551e6b1f3f6d279f7941807dc23b
Night-Parrot <youdewo@163.com>
2020-04-19 18:56:32 +0800

    路由修改

--- src/router.js
+++ src/router.js
@@ -1,11 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+// import Router from './router'
 import Home from './views/Home.vue'
 import testlist from './components/test_list.vue'
 import xm_info from './components/xm_info.vue'
 import about from './views/About.vue'
-import test_page from './components/test.vue'
+import run_times from './components/run_time_page.vue'
 import toolbox from './components/toolbox.vue'
+import login from './components/login.vue'
+import edit_tool from './components/edit_tool.vue'
+import view_tool from './components/view_tool.vue'
+import page_403 from './components/page_403.vue'
+import ctl_wld from './components/control_the_world.vue'
+import test_page2 from './components/test2.vue'
+import test_page3 from './components/test3.vue'
+import tc_list from './components/tc_list.vue'
 
 Vue.use(Router)
 
@@ -35,14 +44,54 @@ export default new Router({
       component: xm_info
     },
     {
-      path: '/testpage',
-      name: '测试页面',
-      component: test_page
+      path: '/run_times',
+      name: '执行次数统计',
+      component: run_times
     },
     {
       path: '/toolbox',
       name: '工具箱',
       component: toolbox
-    }
+    },
+    {
+      path: '/login',
+      name: '登录',
+      component: login
+    },
+    {
+      path: '/edit_tool',
+      name: '文档编辑',
+      component: edit_tool
+    },
+    {
+      path: '/view_tool',
+      name: '文档查看',
+      component: view_tool
+    },
+    {
+      path: '/page_403',
+      name: '权限不足',
+      component: page_403
+    },
+    {
+      path: '/ctl_wld',
+      name: '控制页面',
+      component: ctl_wld
+    },
+    {
+      path: '/testpage2',
+      name: '测试页面2',
+      component: test_page2
+    },    
+    {
+      path: '/testpage3',
+      name: '测试页面3',
+      component: test_page3
+    },    
+    {
+      path: '/tc_list',
+      name: '吐槽页面',
+      component: tc_list
+    },
   ]
 })
