16d7532bcc8b1a3c6168d8eb7269721035703a7f
Night-Parrot <youdewo@163.com>
2020-04-19 18:56:45 +0800

    刷新缓存的页面

---
+++ src/components/control_the_world.vue
@@ -0,0 +1,26 @@
+<template>
+  <div style="margin-top: 10px;margin-left: 10px">
+    <a-button type="primary" @click="relaod_cache">刷新缓存</a-button>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+      relaod_cache() {
+           axios.get("reload_cache")
+          .then(response => {
+            if (response.data.result === "success") {
+                this.$message.success(response.data.msg)
+            } else {
+                this.$message.error(response.data.msg)
+            }
+          });
+      }
+  }
+};
+</script>
