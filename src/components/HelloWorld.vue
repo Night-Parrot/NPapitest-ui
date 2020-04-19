1aa6625d560ca185cee4a48fb80a3f53f3cd8044
Night-Parrot <youdewo@163.com>
2020-04-19 18:57:35 +0800

    删除首页功能区

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,23 @@
 <template>
-  <div>
-    <a-button type="primary" style="float: right;margin-top: 10px; margin-right: 20px" :loading="loading_yhsc" @click="dl_yhsc">下载使用说明</a-button>
+  <div style="padding-top: 50px">
+    <!-- <a-button type="primary" style="float: right;margin-top: 10px; margin-right: 20px" :loading="loading_yhsc" @click="dl_yhsc">下载使用说明</a-button>
+    <a-button type="primary" style="float: right;margin-top: 10px; margin-right: 20px" @click="gototoolbox">工具箱</a-button>
+    <a-button type="primary" style="float: right;margin-top: 10px; margin-right: 20px" @click="updatepwd">修改密码</a-button>
+    <a-modal title="修改密码" v-model="visible_uppwd" @ok="handleOk" @cancel="handleCancel">
+      <a-input-password placeholder="请输入新密码 ԅ(¯﹃¯ԅ)" maxLength="20" v-model="new_pwd" />
+      <br />
+      <br />
+      <a-input-password placeholder="请再输入一遍新密码 （¯﹃¯）" maxLength="20" v-model="new_pwd_se" />
+    </a-modal>
+    <a-button type="primary" style="float: right;margin-top: 10px; margin-right: 20px" @click="logout">退出</a-button>
+    <a-button type="primary" style="float: right;margin-top: 10px; margin-right: 20px" @click="test_page">不要点</a-button> -->
     <div style="float: left;margin-left: 15px;width: 15%;margin-top: 15px;">
       <a-table
         :dataSource="data"
         :columns="columns"
         :loading="loading"
         :pagination="pagination_xmlist"
-        :scroll="{ y: 840}"
+        :scroll="{y: 840}"
         :rowKey="record => record.xmbh"
       >
         <!-- slot-scope 用来绑定record -->
@@ -66,7 +76,13 @@
         </template>
       </a-table>
     </div>
-    <div style="float: left;">
+    <div style="float: left;width: 84%">
+      <a-select defaultValue="用例统计" @change="handleChange" style="right: 90%;margin-top: 10px;" >
+        <a-select-option value="1">用例统计</a-select-option>
+        <a-select-option value="2">项目统计</a-select-option>
+        <a-select-option value="3">开发团队</a-select-option>
+        <a-select-option value="4">测试团队</a-select-option>
+      </a-select>
       <div style="float: left;margin-top: 50px;margin-left:20px;width: 45%;" id="app_c"></div>
       <div style="float: left;margin-top: 50px;margin-left:20px;width: 45%;" id="app_d"></div>
       <div>
@@ -100,20 +116,28 @@
 
 <script>
 import axios from "axios";
-import G2 from "@antv/g2";
+// import G2 from "@antv/g2";
+import { Chart } from '@antv/g2';
 export default {
+  beforeCreate() {
+    this.form = this.$form.createForm(this);
+  },
   mounted() {
-    this.init_fgl();
-    this.init_tgl();
+    this.index_rate(1);
     this.xm_list();
+    this.update_time_order();
+    this.run_time_order();
   },
   data() {
     return {
       data: [],
       loading: false,
+      // visible_uppwd: false,
+      // new_pwd: '',
+      // new_pwd_se: '',
       searchText: "",
       searchInput: null,
-      loading_yhsc: false,
+      // loading_yhsc: false,
       columns: [
         {
           title: "项目名称",
@@ -147,42 +171,11 @@ export default {
           dataIndex: "ylmc"
         },
         {
-          title: "上传时间",
+          title: "更新时间",
           dataIndex: "time"
         }
       ],
-      data_xm: [
-        {
-          key: "1",
-          name: "项目1",
-          ylmc: "立案流程V2.5.4-新增当事人",
-          time: "2019-10-10 13:55:22"
-        },
-        {
-          key: "2",
-          name: "项目2",
-          ylmc: "立案流程V2.5.4-新增当事人",
-          time: "2019-10-10 13:55:22"
-        },
-        {
-          key: "3",
-          name: "项目3",
-          ylmc: "立案流程V2.5.4-新增当事人",
-          time: "2019-10-10 13:55:22"
-        },
-        {
-          key: "4",
-          name: "项目4",
-          ylmc: "立案流程V2.5.4-新增当事人",
-          time: "2019-10-10 13:55:22"
-        },
-        {
-          key: "5",
-          name: "项目5",
-          ylmc: "立案流程V2.5.4-新增当事人",
-          time: "2019-10-10 13:55:22"
-        }
-      ],
+      data_xm: [],
       columns_zx: [
         {
           title: "项目名称",
@@ -205,80 +198,18 @@ export default {
           dataIndex: "time"
         }
       ],
-      data_zx: [
-        {
-          key: "xm_info",
-          name: "项目1",
-          ylmc: "立案流程V2.5.4-新增当事人",
-          tgl: "65%",
-          fgl: "95%",
-          time: "2019-10-10 13:55:22"
-        },
-        {
-          key: "2",
-          name: "项目2",
-          ylmc: "立案流程V2.5.4-新增当事人",
-          tgl: "65%",
-          fgl: "95%",
-          time: "2019-10-10 13:55:22"
-        },
-        {
-          key: "3",
-          name: "项目3",
-          ylmc: "立案流程V2.5.4-新增当事人",
-          tgl: "65%",
-          fgl: "95%",
-          time: "2019-10-10 13:55:22"
-        },
-        {
-          key: "4",
-          name: "项目4",
-          ylmc: "立案流程V2.5.4-新增当事人",
-          tgl: "65%",
-          fgl: "95%",
-          time: "2019-10-10 13:55:22"
-        },
-        {
-          key: "5",
-          name: "项目5",
-          ylmc: "立案流程V2.5.4-新增当事人",
-          tgl: "65%",
-          fgl: "95%",
-          time: "2019-10-10 13:55:22"
-        }
-      ],
+      data_zx: [],
       pagination_xmlist: {
         hideOnSinglePage: true,
         defaultPageSize: 500
       },
       pagination: {
         hideOnSinglePage: true,
-        defaultPageSize: 500
+        defaultPageSize: 5
       },
-      chart_data_c: [
-        { genre: "项目1", sold: "100%", num: 100 },
-        { genre: "项目2", sold: "95.65%", num: 95.65 },
-        { genre: "项目3", sold: "92.65%", num: 92.65 },
-        { genre: "项目4", sold: "85.65%", num: 85.65 },
-        { genre: "项目5", sold: "75%", num: 75 },
-        { genre: "项目6", sold: "72.3%", num: 72.3 },
-        { genre: "项目7", sold: "72.3%", num: 72.3 },
-        { genre: "项目8", sold: "72.3%", num: 72.3 },
-        { genre: "项目9", sold: "72.3%", num: 72.3 },
-        { genre: "项目10", sold: "72.3%", num: 72.3 }
-      ],
-      chart_data_d: [
-        { genre: "项目1", sold: "100%", num: 100 },
-        { genre: "项目2", sold: "95.65%", num: 95.65 },
-        { genre: "项目3", sold: "92.65%", num: 92.65 },
-        { genre: "项目4", sold: "85.65%", num: 85.65 },
-        { genre: "项目5", sold: "75%", num: 75 },
-        { genre: "项目6", sold: "72.3%", num: 72.3 },
-        { genre: "项目7", sold: "72.3%", num: 72.3 },
-        { genre: "项目8", sold: "72.3%", num: 72.3 },
-        { genre: "项目9", sold: "72.3%", num: 72.3 },
-        { genre: "项目10", sold: "72.3%", num: 72.3 }
-      ]
+      chart_data_c: [],
+      chart_data_d: []
+
     };
   },
   methods: {
@@ -302,28 +233,40 @@ export default {
 
     init_fgl() {
       // 此函数为个人习惯,喜欢创建一个初始化的函数
-      const chart = new G2.Chart({
+      const chart = new Chart({
         container: "app_c",
         width: 750,
-        height: 450,
-        padding: [10, 120, 40, 120]
+        height: 400 ,
+        padding: [10, 120, 40, 200]
       });
-      chart.source(this.chart_data_c, { value: { alias: "覆盖率前十的项目" } });
-      chart.axis("value", {
-        // 这部分尚未生效，有待排查
+      chart.data(this.chart_data_c)
+      chart.scale({
+        value: {
+          max: 100,
+          min: 0,
+            alias: '用例平均覆盖率',
+        },
+      });
+      chart.axis('type', {
+        title: null,
+        tickLine: null,
+        line: null,
+      });
+      chart.axis('value', {
         label: null,
         title: {
           offset: 30,
-          textStyle: {
-            fontSize: 10,
-            fontWeight: 600
-          }
-        }
+          style: {
+            fontSize: 12,
+            fontWeight: 300,
+          },
+        },
       });
-      chart.coord().transpose();
+      chart.legend(false);
+      chart.coordinate().transpose();
       chart
         .interval()
-        .position("genre*num")
+        .position("type*value")
         .size(20)
         .label("sold", {
           textStyle: {
@@ -331,18 +274,25 @@ export default {
           },
           offset: 10
         });
-      this.chart = chart;
-      this.chart.render();
+      // this.chart = chart;
+      chart.interaction('element-active');
+      // this.chart.render();
+      chart.render();
     },
     init_tgl() {
       // 此函数为个人习惯,喜欢创建一个初始化的函数
-      const chart = new G2.Chart({
+      const chart = new Chart({
         container: "app_d",
         width: 750,
-        height: 450,
-        padding: [10, 120, 40, 120]
+        height: 400,
+        padding: [10, 120, 40, 200]
       });
-      chart.source(this.chart_data_d, { value: { alias: "覆盖率前十的项目" } });
+      chart.data(this.chart_data_d)
+      chart.scale({
+            value: {
+              alias: '用例平均通过率',
+            },
+        });
       chart.axis("value", {
         // 这部分尚未生效，有待排查
         label: null,
@@ -354,10 +304,10 @@ export default {
           }
         }
       });
-      chart.coord().transpose();
+      chart.coordinate().transpose();
       chart
         .interval()
-        .position("genre*num")
+        .position("type*value")
         .size(20)
         .label("sold", {
           textStyle: {
@@ -372,25 +322,99 @@ export default {
       // alert("这是点击事件")
       alert("key:" + key);
     },
-    dl_yhsc() {
-      this.loading_yhsc = true;
-      axios.get('downloadyhsc', {responseType: 'blob'}).then(response=>{
-        console.log(response);
-        
-        let blob = new Blob([response.data], {
-          type: response.headers["content-type"]
-        });
-        let downloadElement = document.createElement("a");
-        let href = window.URL.createObjectURL(blob); // 创建下载的链接
-        downloadElement.href = href;
-        downloadElement.download = '使用手册.docx'
-        document.body.appendChild(downloadElement);
-        downloadElement.click(); // 点击下载
-        document.body.removeChild(downloadElement); // 下载完成移除元素
-        window.URL.revokeObjectURL(href); // 释放掉blob对象
-      });
-      this.loading_yhsc =false;
-    }
+    // dl_yhsc() {
+    //   this.loading_yhsc = true;
+    //   axios.get('downloadyhsc', {responseType: 'blob'}).then(response=>{
+    //     let blob = new Blob([response.data], {
+    //       type: response.headers["content-type"]
+    //     });
+    //     let downloadElement = document.createElement("a");
+    //     let href = window.URL.createObjectURL(blob); // 创建下载的链接
+    //     downloadElement.href = href;
+    //     downloadElement.download = '使用手册.docx'
+    //     document.body.appendChild(downloadElement);
+    //     downloadElement.click(); // 点击下载
+    //     document.body.removeChild(downloadElement); // 下载完成移除元素
+    //     window.URL.revokeObjectURL(href); // 释放掉blob对象
+    //   });
+    //   this.loading_yhsc =false;
+    // },
+    // logout() {
+    //   axios.get('logout').then(res =>{
+    //     if (res.data.result === "success") {
+    //       this.$cookies.set('status', "logout")
+    //       this.$router.replace({path: '/login'})
+    //     }
+    //   })
+    // },
+    // gototoolbox() {
+    //   window.open('/#/toolbox')
+    // },
+    // updatepwd() {
+    //   this.visible_uppwd = true;
+    // },
+    // handleOk() {
+    //   if (this.new_pwd === this.new_pwd_se) {
+    //     axios.post("updatepwd", {'new_password': this.new_pwd_se}).then(response => {
+    //       if (response.data.result === 'success') {
+    //         this.visible_uppwd = false;
+    //         this.new_pwd = '';
+    //         this.new_pwd_se = '';
+    //         this.$message.success('密码修改成功！✿✿ヽ(°▽°)ノ✿')
+    //       } else(
+    //         this.$message.error(response.data.msg)
+    //       )
+    //     })
+    //   } else(
+    //     this.$message.error('两次密码必须一致！！！o(╯□╰)o')
+    //   )
+    // },
+    handleCancel() {
+      this.new_pwd = '';
+      this.new_pwd_se = '';
+    },
+    update_time_order() {
+      axios.get('update_time_order').then(response => {
+        if (response.status === 200) {
+          this.data_xm = response.data;
+        } else (
+          this.$message.error(response.data.msg)
+        )
+      })
+    },
+    run_time_order() {
+      axios.get('run_time_order').then(response => {
+        if (response.status === 200) {
+          this.data_zx = response.data;
+        } else (
+          this.$message.error(response.data.msg)
+        )
+      })
+    },
+    index_rate(tjfs, limit) {
+      if(!limit){limit=10;}
+      tjfs = parseInt(tjfs)
+      axios.post('index_rate', {tjfs: tjfs, limit: limit}).then(response => {
+        if (response.status === 200) {
+          // this.chart_data = response.data;
+          this.chart_data_c = response.data[0]
+          this.chart_data_d = response.data[1]
+          document.getElementById('app_c').innerHTML = '';
+          document.getElementById('app_d').innerHTML = '';
+          this.init_fgl();
+          this.init_tgl();
+        } else (
+          this.$message.error(response.data.msg)
+        )
+      }
+      )
+    },
+    handleChange(value) {
+      this.index_rate(value)
+    },
+    // test_page() {
+    //   window.open('/#/testpage')
+    // }
   }
 };
 </script>
