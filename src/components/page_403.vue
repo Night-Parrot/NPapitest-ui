1b1133cc3810fc2e4987a53a9f969fda61b3bfb1
Night-Parrot <youdewo@163.com>
2020-04-19 18:58:02 +0800

    403页面

---
+++ src/components/page_403.vue
@@ -0,0 +1,20 @@
+<template>
+  <a-result status="403" subTitle="你已经被封印了！щ(゜ロ゜щ)">
+    <template v-slot:extra>
+      <a-button type="primary" @click="back_index" >解除封印！(*￣∇￣*)</a-button>
+    </template>
+  </a-result>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+      back_index() {
+          this.$router.replace({path: '/'})
+      }
+  }
+};
+</script>
